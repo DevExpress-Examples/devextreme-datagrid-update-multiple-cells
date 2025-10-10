8b5290b437c38830d148240620d30e57cd252200
Charles Vincent Cordial (DevExpress) <charles.cordial@devsoft.ph>
2025-10-10 18:09:19 +0800

    Fix: jQuery implementation, lint, and build to v25.1.3

--- jQuery/src/index.js
+++ jQuery/src/index.js
@@ -1,10 +1,41 @@
 $(() => {
-  let count = 0;
-  $('#btn').dxButton({
-    text: `Click count: ${count}`,
-    onClick(e) {
-      count += 1;
-      e.component.option('text', `Click count: ${count}`);
+  $('#gridContainer').dxDataGrid({
+    keyExpr: 'ID',
+    dataSource: employees,
+    editing: {
+      allowUpdating: true,
+      allowAdding: true,
+      mode: 'row',
+    },
+    columns: [
+      {
+        caption: 'Name',
+        dataField: 'CustomerID',
+        setCellValue: (rowData, value) => {
+          if (!rowData || !value) return;
+
+          rowData.CustomerID = value.CustomerID;
+          rowData.Address = value.Address;
+          rowData.Phone = value.Phone;
+        },
+        lookup: {
+          dataSource: customers,
+          valueExpr: 'CustomerID',
+          displayExpr: 'CustomerName',
+        },
+      },
+      'Address',
+      'Phone',
+    ],
+    onEditorPreparing: (e) => {
+      if (e.parentType === 'dataRow' && e.dataField === 'CustomerID') {
+        e.editorOptions.onValueChanged = (ev) => {
+          const selectedItem = ev.component.option('selectedItem');
+          if (!selectedItem || !e.setValue) return;
+
+          e.setValue(selectedItem);
+        };
+      }
     },
   });
 });
