41f7b3b7e30240a014371d31e0e3c65f4c6e673c
Charles Vincent Cordial (DevExpress) <charles.cordial@devsoft.ph>
2025-10-10 18:09:38 +0800

    Fix: Angular implementation, lint, and build to v25.1.3

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,20 +1,40 @@
-import { Component } from '@angular/core';
-import { ClickEvent } from 'devextreme/ui/button';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import type { Customer, Employee } from './types';
+import { DxDataGridComponent, type DxDataGridTypes } from 'devextreme-angular/ui/data-grid';
+import type { ValueChangedEvent } from 'devextreme/ui/lookup';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Angular';
+export class AppComponent implements OnInit {
+  @ViewChild(DxDataGridComponent) dataGrid!: DxDataGridComponent<Employee, number>;
+  employees: Employee[] = [];
+  customers: Customer[] = [];
 
-  counter = 0;
+  constructor(private service: AppService) {}
 
-  buttonText = 'Click count: 0';
+  ngOnInit(): void {
+    this.employees = this.service.getEmployees();
+    this.customers = this.service.getCustomers();
+  }
 
-  onClick(e: ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  onEditorPreparing(e: DxDataGridTypes.EditorPreparingEvent<Employee, number>): void {
+    if (e.parentType === 'dataRow' && e.dataField === 'CustomerID') {
+      e.editorOptions.onValueChanged = (ev: ValueChangedEvent): void => {
+        const selectedItem = ev.component.option('selectedItem') as Customer;
+        if (!selectedItem || !e.setValue) return;
+        e.setValue(selectedItem);
+      };
+    }
+  }
+
+  setCellValue(rowData: Employee, value: Customer): void {
+    if (!rowData || !value) return;
+    rowData.CustomerID = value.CustomerID;
+    rowData.Address = value.Address;
+    rowData.Phone = value.Phone;
   }
 }
---
+++ Angular/src/app/types.ts
@@ -0,0 +1,13 @@
+export interface Customer {
+    CustomerID: number;
+    CustomerName: string;
+    Address: string;
+    Phone: string;
+}
+
+export interface Employee {
+    ID: number;
+    CustomerID: number;
+    Address: string;
+    Phone: string;
+}
