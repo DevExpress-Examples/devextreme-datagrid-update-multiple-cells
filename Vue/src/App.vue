0d2ff57b9a866db5d938842b43d2f7dadc7ccdf4
Charles Vincent Cordial (DevExpress) <charles.cordial@devsoft.ph>
2025-10-10 18:10:01 +0800

    Fix: Vue implementation, lint, and build to v25.1.3

--- Vue/src/App.vue
+++ Vue/src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.css';
+// Router will mount views; HomeView renders HomeContent
 </script>
 
 <template>
-  <div class="main">
-    <RouterView/>
-  </div>
+  <router-view/>
 </template>
